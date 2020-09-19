e6f2c090603f88be84d1c994e7a30bd4e317a631
cw-amitsingh <51703039+cw-amitsingh@users.noreply.github.com>
2020-09-19 18:08:36 +0530

    bypass linkvertise with r qs

---
+++ src/constants/index.js
@@ -0,0 +1,1 @@
+export const LINKVERTISE = "linkvertise.com";
--- src/utils/bypassLinks.js
+++ src/utils/bypassLinks.js
@@ -1,7 +1,24 @@
-/* eslint-disable */
+import { LINKVERTISE } from "../constants";
+import { bypassLinkvertise } from "./bypassLinkvertise";
+
+const handleTargetUrl = (tab, targetUrl) => {
+  //eslint-disable-next-line no-undef
+  targetUrl && chrome.tabs.update(tab.id, { url: targetUrl });
+};
 
 export const bypassLink = () => {
+  let currentTabUrl;
+  let targetUrl;
+  //eslint-disable-next-line no-undef
   chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
-    const url = tabs[0].url;
+    currentTabUrl = new URL(tabs[0].url);
+    switch (currentTabUrl.hostname) {
+      case LINKVERTISE:
+        targetUrl = bypassLinkvertise(currentTabUrl);
+        break;
+      default:
+        targetUrl = null;
+    }
+    handleTargetUrl(tabs, targetUrl);
   });
 };
