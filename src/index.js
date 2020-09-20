c183213fa961bc28f3b851563c9606e6561e0fa1
cw-amitsingh <51703039+cw-amitsingh@users.noreply.github.com>
2020-09-20 17:06:41 +0530

    fixed gh pages build

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { createMuiTheme, ThemeProvider, CssBaseline } from "@material-ui/core";
+
+const theme = createMuiTheme({
+  palette: {
+    type: "dark",
+  },
+});
 
 ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <ThemeProvider theme={theme}>
+    <StrictMode>
+      <CssBaseline />
+      <App />
+    </StrictMode>
+  </ThemeProvider>,
   document.getElementById("root")
 );
