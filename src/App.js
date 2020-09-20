4be7c82cbb2281e57b08687d83ea5c3068507ac9
cw-amitsingh <51703039+cw-amitsingh@users.noreply.github.com>
2020-09-20 16:36:30 +0530

    refactored code and removed useless code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <header>
         <p>Bypass Links. Enjoy!!!</p>
       </header>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
   document.getElementById("root")
 );
