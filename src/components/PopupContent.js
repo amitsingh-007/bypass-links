a8c2de0c29e7d8be0b80aee807c54adad735993e
cw-amitsingh <51703039+cw-amitsingh@users.noreply.github.com>
2020-11-17 22:58:25 +0530

    added feature to clear history

--- src/components/PopupContent.js
+++ src/components/PopupContent.js
@@ -1,26 +1,75 @@
-import { Box, Typography } from "@material-ui/core";
+import { Box, Switch, Typography } from "@material-ui/core";
 import { green } from "@material-ui/core/colors";
 import CancelTwoToneIcon from "@material-ui/icons/CancelTwoTone";
 import CheckCircleTwoToneIcon from "@material-ui/icons/CheckCircleTwoTone";
 import React, { useEffect, useState } from "react";
 import { EXTENSION_STATE } from "../constants";
 
+const SWITCH_INPUT_PROPS = {
+  "aria-label": "primary checkbox",
+};
+const switchTextStyles = {
+  fontSize: "19px",
+  paddingRight: "12px",
+};
+
 export const PopupContent = () => {
   const [extState, setExtState] = useState("...");
+  const [isHistoryWatchActive, setIsHistoryWatchActive] = useState(false);
 
   useEffect(() => {
-    chrome.storage.sync.get(["extState"], ({ extState }) => {
-      console.log(`Extension currently is ${extState}.`);
-      setExtState(extState);
-    });
-  });
+    if (!__IS_BROWSER__) {
+      chrome.storage.sync.get(
+        ["extState", "historyStartTime"],
+        ({ extState, historyStartTime }) => {
+          console.log(`Extension currently is ${extState}.`);
+          setExtState(extState);
+          setIsHistoryWatchActive(!!historyStartTime);
+        }
+      );
+    }
+  }, []);
+
+  const handleHistorySwitchChange = (event) => {
+    const isActive = event.target.checked;
+    console.log(isActive);
+    setIsHistoryWatchActive(isActive);
+    if (__IS_BROWSER__) {
+      return;
+    }
+    if (isActive) {
+      const historyStartTime = Date.now();
+      chrome.storage.sync.set({ historyStartTime }, () => {
+        console.log(`historyStartTime is set to ${historyStartTime}.`);
+      });
+    } else {
+      chrome.storage.sync.get(["historyStartTime"], ({ historyStartTime }) => {
+        if (!historyStartTime) {
+          console.error("No start date time to clear history.");
+        }
+        const historyEndTime = Date.now();
+        console.log(`Start DateTime is: ${new Date(historyStartTime)}`);
+        console.log(`End DateTime is: ${new Date(historyEndTime)}`);
+        chrome.history.deleteRange(
+          {
+            startTime: historyStartTime,
+            endTime: historyEndTime,
+          },
+          () => {
+            console.log("History cleared.");
+            setIsHistoryWatchActive(false);
+            chrome.storage.sync.remove("historyStartTime");
+          }
+        );
+      });
+    }
+  };
 
   return (
     <Box
       display="flex"
       flexDirection="column"
       alignItems="center"
-      height="80px"
       width="200px"
       padding="14px"
     >
@@ -37,6 +86,20 @@ export const PopupContent = () => {
       ) : (
         <CancelTwoToneIcon color="secondary" fontSize="large" />
       )}
+      <Box marginTop="8.4px">
+        <Switch
+          checked={isHistoryWatchActive}
+          onChange={handleHistorySwitchChange}
+          color="primary"
+          name="historyWatch"
+          inputProps={SWITCH_INPUT_PROPS}
+        />
+        <Box component="span" display="inline-block">
+          <Typography variant="h5" component="h5" style={switchTextStyles}>
+            {isHistoryWatchActive ? "Watching" : "Inactive"}
+          </Typography>
+        </Box>
+      </Box>
     </Box>
   );
 };
