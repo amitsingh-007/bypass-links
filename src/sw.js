c175a0a18a970c6de5d855a6b681b99c70bb8ca2
cw-amitsingh <51703039+cw-amitsingh@users.noreply.github.com>
2020-12-15 21:12:17 +0530

    cache static assets

--- src/sw.js
+++ src/sw.js
@@ -5,12 +5,41 @@ import { registerRoute } from "workbox-routing";
 import { NetworkFirst } from "workbox-strategies";
 
 self.skipWaiting();
+
+//Cache build time assets(html, js, css)
 precacheAndRoute(self.__WB_MANIFEST);
 
+//Cache extension zip file
 registerRoute(
   /.*(bypass-links-.*).*\.zip$/,
   new NetworkFirst({
     cacheName: "extension-file-cache",
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 1,
+      }),
+    ],
+  })
+);
+
+//Cache fonts
+registerRoute(
+  /https:\/\/fonts\.gstatic.com\/.*\.woff2/,
+  new NetworkFirst({
+    cacheName: "fonts-cache",
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 1,
+      }),
+    ],
+  })
+);
+
+//Cache AddToHomeScreen assets
+registerRoute(
+  /manifest\.webmanifest\.json|bypass_link_512\.png|bypass_link_192\.png/,
+  new NetworkFirst({
+    cacheName: "A2HS-cache",
     plugins: [
       new ExpirationPlugin({
         maxEntries: 1,
