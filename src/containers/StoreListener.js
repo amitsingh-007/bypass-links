60f60164e5115c5b4ab4f823dbc470c05b8f9c12
cw-amitsingh <51703039+cw-amitsingh@users.noreply.github.com>
2021-01-14 20:08:33 +0530

    fix history clear issue

--- src/containers/StoreListener.js
+++ src/containers/StoreListener.js
@@ -3,7 +3,15 @@ import { memo, useEffect } from "react";
 import { useSelector } from "react-redux";
 
 const THIRTY_SECONDS = 30 * 1000; //in milliseconds
+
+const isHistoryAlreadyActive = async () => {
+  const { historyStartTime } = await storage.get(["historyStartTime"]);
+  return Boolean(historyStartTime);
+};
 export const startHistoryWatch = async () => {
+  if (await isHistoryAlreadyActive()) {
+    return;
+  }
   await storage.set({
     historyStartTime: new Date() - THIRTY_SECONDS, //to compensate for open defaults
   });
