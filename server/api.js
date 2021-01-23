d1791e3eb2d391e1bfcdd897302180bc6a9ebe90
cw-amitsingh <51703039+cw-amitsingh@users.noreply.github.com>
2021-01-23 20:15:08 +0530

    added severless funtions deploy on netlify

---
+++ server/api.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const serverless = require("serverless-http");
+
+const app = express();
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.json({
+    hello: "hi!",,
+  });
+});
+
+app.use(`/.netlify/functions/api`, router);
+
+module.exports = app;
+module.exports.handler = serverless(app);
