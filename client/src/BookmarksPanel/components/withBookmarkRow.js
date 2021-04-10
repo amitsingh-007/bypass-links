bd1dc5f41a45466600da0a97a9e6fe6fe60bab07
Amit Singh <amit.singh@carwale.com>
2021-04-10 23:28:54 +0530

    fixed bookmarks panel right click

--- client/src/BookmarksPanel/components/withBookmarkRow.js
+++ client/src/BookmarksPanel/components/withBookmarkRow.js
@@ -1,45 +1,20 @@
-import { Box, MenuItem } from "@material-ui/core";
-import { BlackMenu } from "GlobalComponents/StyledComponents";
+import { Box } from "@material-ui/core";
 import { memo } from "react";
 import { Draggable } from "react-beautiful-dnd";
 import Ripples from "react-ripples";
-import useMenu from "SrcPath/hooks/useMenu";
 import { bookmarkRowStyles } from "../constants";
 
 const withBookmarkRow = (Component) =>
   memo((props) => {
-    const [isMenuOpen, menuPos, onMenuClose, onMenuOpen] = useMenu();
-
-    const renderMenu = (menuItemOptionList) => (
-      <BlackMenu
-        open={isMenuOpen}
-        onClose={onMenuClose}
-        anchorReference="anchorPosition"
-        anchorPosition={menuPos}
-      >
-        {menuItemOptionList.map(({ text, onClick }) => (
-          <MenuItem
-            key={text}
-            onClick={() => {
-              onClick();
-              onMenuClose();
-            }}
-          >
-            {text}
-          </MenuItem>
-        ))}
-      </BlackMenu>
-    );
-
     const { isDir, name, url, pos, title } = props;
     const primaryUniqueId = isDir ? name : url;
     const secondaryUniqueId = isDir ? null : title;
+
     return (
       <Draggable draggableId={primaryUniqueId} index={pos}>
         {(provided) => (
           <Box
             className="bookmarkRowContainer"
-            onContextMenu={onMenuOpen}
             ref={provided.innerRef}
             sx={{
               display: "flex",
@@ -60,11 +35,7 @@ const withBookmarkRow = (Component) =>
                   maxWidth: "747px",
                 }}
               >
-                <Component
-                  {...props}
-                  renderMenu={renderMenu}
-                  containerStyles={bookmarkRowStyles}
-                />
+                <Component {...props} containerStyles={bookmarkRowStyles} />
               </Box>
             </Ripples>
           </Box>
