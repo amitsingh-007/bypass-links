e3a301b80e743ead426fbfef74909c9e14493243
cw-amitsingh <amit.singh@carwale.com>
2021-05-30 18:46:23 +0530

    fix images not seeing in bookmarks panel

--- client/src/TaggingPanel/components/PersonAvatars.js
+++ client/src/TaggingPanel/components/PersonAvatars.js
@@ -11,7 +11,8 @@ const commonStyles = { marginRight: "8px" };
 const PersonAvatars = memo(({ persons }) => {
   const history = useHistory();
 
-  const hasImages = persons?.length && persons.every(({ url }) => Boolean(url));
+  const hasImages =
+    persons?.length && persons.every(({ imageUrl }) => Boolean(imageUrl));
 
   if (!hasImages) {
     return (
