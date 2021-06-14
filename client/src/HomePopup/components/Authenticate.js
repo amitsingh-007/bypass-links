575a7b8767114c8830505144410d006274719b4b
amitsingh-007 <amit.singh@carwale.com>
2021-06-14 22:45:42 +0530

    disable popup on sign in or sign out

--- client/src/HomePopup/components/Authenticate.js
+++ client/src/HomePopup/components/Authenticate.js
@@ -1,4 +1,11 @@
-import { Box, IconButton, LinearProgress, Typography } from "@material-ui/core";
+import {
+  Box,
+  Dialog,
+  DialogTitle,
+  IconButton,
+  LinearProgress,
+  Typography,
+} from "@material-ui/core";
 import CloudDoneTwoToneIcon from "@material-ui/icons/CloudDoneTwoTone";
 import CloudOffTwoTone from "@material-ui/icons/CloudOffTwoTone";
 import {
@@ -71,7 +78,20 @@ const Authenticate = memo(() => {
     return (
       <>
         <IconButtonLoader />
-        <Box sx={{ position: "fixed", top: 0, left: 0, width: "100%" }}>
+        <Dialog
+          sx={{
+            ".MuiPaper-root": {
+              m: 0,
+              top: 0,
+              width: "100%",
+              position: "fixed",
+              borderRadius: "0px",
+              backgroundColor: "unset",
+              backgroundImage: "unset",
+            },
+          }}
+          open
+        >
           <LinearProgress
             variant="buffer"
             value={(progress * 100) / total}
@@ -88,7 +108,7 @@ const Authenticate = memo(() => {
           >
             {message || "Loading"}
           </Typography>
-        </Box>
+        </Dialog>
       </>
     );
   }
