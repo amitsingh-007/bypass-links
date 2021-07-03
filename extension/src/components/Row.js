ec7787e10edf414099f4514f2666ea2b12f57b4f
amitsingh-007 <amit.singh@carwale.com>
2021-07-03 13:12:43 +0530

    change home popup styles

--- extension/src/components/Row.js
+++ extension/src/components/Row.js
@@ -1,5 +1,15 @@
 import { Box } from "@material-ui/core";
 
-export const Row = ({ children }) => (
-  <Box sx={{ marginTop: "8.4px" }}>{children}</Box>
+export const Row = ({ children, styles = {} }) => (
+  <Box
+    sx={{
+      display: "flex",
+      justifyContent: "space-between",
+      width: "100%",
+      my: "3px",
+      ...styles,
+    }}
+  >
+    {children}
+  </Box>
 );
