6a4c5cb2f1b222bc41c3f34b899ec966787a37ff
amitsingh-007 <amit.singh@carwale.com>
2021-08-21 17:34:10 +0530

    Update index.tsx

--- extension/src/index.tsx
+++ extension/src/index.tsx
@@ -1,6 +1,6 @@
 // Uncomment this to see render comments in the console
 if (!__PROD__) {
-  require("../scripts/wdyr");
+  require("./scripts/wdyr");
 }
 import { CssBaseline } from "@material-ui/core";
 import darkScrollbar from "@material-ui/core/darkScrollbar";
