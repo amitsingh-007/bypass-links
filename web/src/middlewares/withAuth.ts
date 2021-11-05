334e325ffe6c64b647c26d1682c35ebda9fef2ed
amitsingh-007 <amit.singh@carwale.com>
2021-11-05 22:39:28 +0530

    remove compression middleware

--- web/src/middlewares/withAuth.ts
+++ web/src/middlewares/withAuth.ts
@@ -1,5 +1,4 @@
 import { ALLOWED_ORIGIN } from "@constants/index";
-import compression from "compression";
 import cors from "cors";
 import rateLimit from "express-rate-limit";
 import helmet from "helmet";
@@ -17,13 +16,12 @@ const withAuth =
       windowMs: 5 * 60 * 1000, // 5 minutes
       max: 20,
     });
-    runMiddleware(req, res, helmet());
-    runMiddleware(req, res, compression());
-    runMiddleware(req, res, apiLimiter);
+    await runMiddleware(req, res, helmet());
+    await runMiddleware(req, res, apiLimiter);
     if (__PROD__) {
-      runMiddleware(req, res, cors({ origin: ALLOWED_ORIGIN }));
+      await runMiddleware(req, res, cors({ origin: ALLOWED_ORIGIN }));
     }
-    runMiddleware(req, res, verifyUserId);
+    await runMiddleware(req, res, verifyUserId);
     return handler(req, res);
   };
 
