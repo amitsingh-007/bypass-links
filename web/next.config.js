4dcd845fb476595a6c5b15a8d3023b1d0517520d
Amit Singh <amitsingh5198@gmail.com>
2022-04-30 22:50:53 +0530

    Update next.config.js

--- web/next.config.js
+++ web/next.config.js
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require("path");
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 const ESLintPlugin = require("eslint-webpack-plugin");
