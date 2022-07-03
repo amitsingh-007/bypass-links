35dca74e5e88657cf2ba5af6b208f7a8df23a08e
Amit Singh <amitsingh5198@gmail.com>
2022-07-03 16:59:09 +0530

    Fix build

--- extension/src/helpers/chrome/scripting.ts
+++ extension/src/helpers/chrome/scripting.ts
@@ -1,7 +1,7 @@
 import promisify from './promisifyChromeApi';
 
 const scripting = {
-  executeScript: (options: chrome.scripting.ScriptInjection) =>
+  executeScript: (options: chrome.scripting.ScriptInjection<any>) =>
     promisify<chrome.scripting.InjectionResult[]>((callback?: any) => {
       chrome.scripting.executeScript(options, callback);
     }),
