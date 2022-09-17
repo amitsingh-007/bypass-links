b0e5710922d33e6ad2cff1a477c05ced3576ab16
Amit Singh <amitsingh5198@gmail.com>
2022-09-17 12:42:26 +0530

    Remove cors from apis

--- web/src/middlewares/withAuth.ts
+++ web/src/middlewares/withAuth.ts
@@ -1,5 +1,3 @@
-import { ALLOWED_ORIGIN } from '@constants/index';
-import cors from 'cors';
 import helmet from 'helmet';
 import { NextApiRequest, NextApiResponse } from 'next';
 import runMiddleware from './runMiddleware';
@@ -12,9 +10,6 @@ interface Handler {
 const withAuth =
   (handler: Handler) => async (req: NextApiRequest, res: NextApiResponse) => {
     await runMiddleware(req, res, helmet());
-    if (__PROD__) {
-      await runMiddleware(req, res, cors({ origin: ALLOWED_ORIGIN }));
-    }
     await runMiddleware(req, res, verifyUserId);
     return handler(req, res);
   };
