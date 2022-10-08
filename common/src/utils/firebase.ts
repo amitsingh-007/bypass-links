1c6b04b3e9f73c0922270ef74802083d80a648d7
Amit Singh <amitsingh5198@gmail.com>
2022-10-08 23:30:02 +0530

    Abstract out firebase config

--- common/src/utils/firebase.ts
+++ common/src/utils/firebase.ts
@@ -11,3 +11,14 @@ export const getFullDbPath = (
   const env = getEnv();
   return `${env}/${uid}/${ref}`;
 };
+
+export const getPublicConfig = () => ({
+  apiKey: 'AIzaSyDiMRlBhW36sLjEADoQj9T5L1H-hIDUAso',
+  authDomain: 'bypass-links.firebaseapp.com',
+  databaseURL: 'https://bypass-links.firebaseio.com/',
+  projectId: 'bypass-links',
+  storageBucket: 'bypass-links.appspot.com',
+  messagingSenderId: '603462573180',
+  appId: '1:603462573180:web:317c7f02f1f66b836f3df9',
+  measurementId: 'G-ZGKPZFJ01Z',
+});
