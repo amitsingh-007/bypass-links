6dbfef9ba17be19af13edd4b1bae02837a16d863
Amit Singh <amitsingh5198@gmail.com>
2022-11-19 18:29:37 +0530

    Bump nextjs version

--- web/next.config.js
+++ web/next.config.js
@@ -12,7 +12,6 @@ const isDev = process.env.NODE_ENV === 'development';
  **/
 const nextConfig = {
   productionBrowserSourceMaps: true,
-  swcMinify: true,
   compiler: {
     removeConsole: isDev ? false : { exclude: ['error'] },
   },
