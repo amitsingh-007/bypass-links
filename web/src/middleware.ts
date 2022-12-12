be17324f464804da0287bacaeecdeb197e25689a
Amit Singh <amitsingh5198@gmail.com>
2022-12-12 22:27:52 +0530

    Update middleware.ts

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export function middleware(req: NextRequest, _res: NextResponse) {
+export function middleware(req: NextRequest) {
   if (req.nextUrl.pathname === '/') {
     const { geo, nextUrl } = req;
     nextUrl.searchParams.set('country', geo?.country ?? 'IN');
