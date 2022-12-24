72b585a879e6ab32ab5f065ad97169be29be10c9
Amit Singh <amitsingh5198@gmail.com>
2022-12-24 16:05:45 +0530

    Remove row comp

--- extension/src/HomePopup/containers/PopupHome.tsx
+++ extension/src/HomePopup/containers/PopupHome.tsx
@@ -1,4 +1,4 @@
-import { Box, Text } from '@mantine/core';
+import { Box, Grid, Text } from '@mantine/core';
 import { memo } from 'react';
 import Authenticate from '../components/Authenticate';
 import BookmarksPanelButton from '../components/BookmarksPanelButton';
@@ -8,7 +8,6 @@ import OpenDefaultsButton from '../components/OpenDefaultsButton';
 import OpenForumLinks from '../components/OpenForumLinks';
 import PersonsPanelButton from '../components/PersonsPanelButton';
 import QuickBookmarkButton from '../components/QuickBookmarkButton';
-import { Row } from '../components/Row';
 import ShortcutsPanelButton from '../components/ShortcutsPanelButton';
 import ToggleExtension from '../components/ToggleExtension';
 import ToggleHistory from '../components/ToggleHistory';
@@ -24,7 +23,6 @@ const PopupHome = memo(function PopupHome() {
           flexDirection: 'column',
           alignItems: 'center',
           padding: '8px 16px 16px',
-          width: '218px',
         }}
       >
         <Text
@@ -58,21 +56,35 @@ const PopupHome = memo(function PopupHome() {
           </Box>
           <UserProfile />
         </Box>
-        <Row styles={{ marginTop: '15px' }}>
-          <Authenticate />
-          <ShortcutsPanelButton />
-          <BookmarksPanelButton />
-        </Row>
-        <Row>
-          <HistoryPanelButton />
-          <OpenDefaultsButton />
-          <QuickBookmarkButton />
-        </Row>
-        <Row>
-          <OpenForumLinks />
-          <LastVisitedButton />
-          <PersonsPanelButton />
-        </Row>
+        <Grid gutter="lg" sx={{ marginTop: '10px' }}>
+          <Grid.Col span={4}>
+            <Authenticate />
+          </Grid.Col>
+          <Grid.Col span={4}>
+            <ShortcutsPanelButton />
+          </Grid.Col>
+          <Grid.Col span={4}>
+            <BookmarksPanelButton />
+          </Grid.Col>
+          <Grid.Col span={4}>
+            <HistoryPanelButton />
+          </Grid.Col>
+          <Grid.Col span={4}>
+            <OpenDefaultsButton />
+          </Grid.Col>
+          <Grid.Col span={4}>
+            <QuickBookmarkButton />
+          </Grid.Col>
+          <Grid.Col span={4}>
+            <OpenForumLinks />
+          </Grid.Col>
+          <Grid.Col span={4}>
+            <LastVisitedButton />
+          </Grid.Col>
+          <Grid.Col span={4}>
+            <PersonsPanelButton />
+          </Grid.Col>
+        </Grid>
       </Box>
       <TwoFactorAuthenticate />
     </>
