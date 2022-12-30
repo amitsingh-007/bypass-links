8bd59b0ad0e6de39fc783e28436fd0b2093c46a3
Amit Singh <amitsingh5198@gmail.com>
2022-12-30 15:50:22 +0530

    Remove mui from extension & shared project

--- extension/src/index.tsx
+++ extension/src/index.tsx
@@ -1,49 +1,11 @@
 import { mantineTheme } from '@bypass/shared/constants/theme';
 import { MantineProvider } from '@mantine/core';
-import { CssBaseline } from '@mui/material';
-import darkScrollbar from '@mui/material/darkScrollbar';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import Global from './components/Global';
 import PopupRoutes from './components/PopupRoutes';
 import DynamicProvider from './provider/DynamicProvider';
-
-const theme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-  typography: {
-    fontFamily: `"Inter", sans-serif`,
-  },
-  components: {
-    MuiCssBaseline: {
-      styleOverrides: {
-        body: darkScrollbar(),
-      },
-    },
-    MuiButton: {
-      styleOverrides: {
-        outlined: {
-          borderWidth: '2px',
-          borderRadius: '50px',
-          ':hover': { borderWidth: '2px' },
-        },
-        root: {
-          '&.Mui-disabled': {
-            borderWidth: '2px',
-          },
-        },
-      },
-    },
-    MuiTextField: {
-      defaultProps: {
-        autoComplete: 'off',
-      },
-    },
-  },
-});
 
 const container = document.getElementById('root');
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
@@ -52,15 +14,12 @@ root.render(
   <StrictMode>
     <BrowserRouter>
       <MantineProvider withGlobalStyles withNormalizeCSS theme={mantineTheme}>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <DynamicProvider>
-            <>
-              <PopupRoutes />
-              <Global />
-            </>
-          </DynamicProvider>
-        </ThemeProvider>
+        <DynamicProvider>
+          <>
+            <PopupRoutes />
+            <Global />
+          </>
+        </DynamicProvider>
       </MantineProvider>
     </BrowserRouter>
   </StrictMode>
