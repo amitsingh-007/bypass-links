4efd4c266d3fc5d9b46d2e11efec3ff5ab8fa279
Amit Singh <amitsingh5198@gmail.com>
2023-01-01 17:46:53 +0530

    Migrate to turborepo

--- packages/shared/config/.eslintrc.js
+++ packages/shared/config/.eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'plugin:react/jsx-runtime',
     'prettier',
+    'turbo',
   ],
   plugins: ['import'],
   parser: '@typescript-eslint/parser',
