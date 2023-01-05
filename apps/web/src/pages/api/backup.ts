86a7f8c8b8371047bbfa5ed6abd685977c6e050e
Amit Singh <amitsingh5198@gmail.com>
2023-01-05 19:09:58 +0530

    remove old middlewares

--- apps/web/src/pages/api/backup.ts
+++ apps/web/src/pages/api/backup.ts
@@ -1,17 +1,12 @@
 import { backupData } from '@bypass/trpc';
-import bearerToken from 'express-bearer-token';
 import { NextApiRequest, NextApiResponse } from 'next';
-import runMiddleware from 'src/middlewares/runMiddleware';
 
 type NextApiRequestWithToken = NextApiRequest & {
   token?: string;
 };
 
 const handler = async (req: NextApiRequestWithToken, res: NextApiResponse) => {
-  runMiddleware(req, res, bearerToken());
-
-  const authBearerToken = req.token;
-  console.log(process.env.FIREBASE_BACKUP_CRON_JOB_API_KEY);
+  const authBearerToken = req.query['access_token'] as string;
   if (authBearerToken !== process.env.FIREBASE_BACKUP_CRON_JOB_API_KEY) {
     return res.status(401).end();
   }
