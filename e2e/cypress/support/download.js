f4108f185eeb7959d4934f980fed2435d70a8a39
Amit Singh <amitsingh5198@gmail.com>
2023-01-06 23:22:25 +0530

    Remove cypress from code

--- e2e/cypress/support/download.js
+++
@@ -1,8 +0,0 @@
-Cypress.Commands.add('verifyExtensionDownload', () => {
-  cy.get(`[data-test-attr='ext-download-button']`)
-    .should('have.attr', 'href')
-    .and(
-      'match',
-      /https:\/\/github\.com\/amitsingh-007\/bypass-links\/releases\/download\/.*\.zip/
-    );
-});
