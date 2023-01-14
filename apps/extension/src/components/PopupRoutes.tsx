b39e9c179ccb7747a7652f104beb6e5ad6a3506e
Amit Singh <amitsingh5198@gmail.com>
2023-01-14 16:26:34 +0530

    Update PopupRoutes.tsx

--- apps/extension/src/components/PopupRoutes.tsx
+++ apps/extension/src/components/PopupRoutes.tsx
@@ -1,22 +1,9 @@
 import { Suspense } from 'react';
 import { Routes } from 'react-router-dom';
-import { BookmarksPanelRoute } from '@/BookmarksPanel/routes';
-import { HistoryPanelRoute } from '@/HistoryPanel/routes';
-import { HomePageRoute } from '@/HomePopup/routes';
-import { PersonsPanelRoute } from '@/PersonsPanel/routes';
-import { SettingsPanelRoute } from '@/SettingsPanel/routes';
-import { ShortcutsPanelRoute } from '@/ShortcutsPanel/routes';
 
 const PopupRoutes = () => (
   <Suspense fallback={null}>
-    <Routes>
-      {HomePageRoute}
-      {ShortcutsPanelRoute}
-      {BookmarksPanelRoute}
-      {PersonsPanelRoute}
-      {HistoryPanelRoute}
-      {SettingsPanelRoute}
-    </Routes>
+    <Routes></Routes>
   </Suspense>
 );
 
