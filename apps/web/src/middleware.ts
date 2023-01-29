668ba826ed85824544a0f5d4cdd13ff70e71fb60
Amit Singh <amitsingh5198@gmail.com>
2023-01-29 12:59:53 +0530

    change logs

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,33 +1,32 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-// @ts-ignore
-import requestIp from 'request-ip';
 
 export function middleware(req: NextRequest) {
   if (req.nextUrl.pathname === '/') {
-    const detectedIp = requestIp.getClientIp(req);
-    console.log('ip', detectedIp);
-    // @ts-ignore
-    const forwarded = req.headers['x-forwarded-for'];
-    console.log('forwarded', forwarded);
-    // @ts-ignore
-    console.log('remoteAddress', req?.socket?.remoteAddress);
-    console.log('headers', req.headers);
-    // const ip =
+    // const detectedIp = requestIp.getClientIp(req);
+    // console.log('ip', detectedIp);
+    // // @ts-ignore
+    // const forwarded = req.headers['x-forwarded-for'];
+    // console.log('forwarded', forwarded);
+    // // @ts-ignore
+    // console.log('remoteAddress', req?.socket?.remoteAddress);
+    // console.log('headers', req.headers);
+    // const compIp =
     //   typeof forwarded === 'string'
     //     ? forwarded.split(/, /)[0]
-    //     : req.socket.remoteAddress;
-    // console.log('ip', { ip });
+    //     : // @ts-ignore
+    //       req?.socket?.remoteAddress;
+    // console.log('compIp', compIp);
     const { geo, nextUrl } = req;
     nextUrl.searchParams.set('country', geo?.country ?? 'IN');
-    nextUrl.searchParams.set('ip', detectedIp ?? '');
-    // @ts-ignore
-    nextUrl.searchParams.set('remoteAddress', req?.socket?.remoteAddress ?? '');
-    nextUrl.searchParams.set(
-      'forwarded',
-      typeof forwarded === 'string' ? forwarded : ''
-    );
+    // nextUrl.searchParams.set('ip', detectedIp ?? '');
+    // // @ts-ignore
+    // nextUrl.searchParams.set('remoteAddress', req?.socket?.remoteAddress ?? '');
+    // nextUrl.searchParams.set(
+    //   'forwarded',
+    //   typeof forwarded === 'string' ? forwarded : ''
+    // );
     return NextResponse.rewrite(nextUrl);
   }
   return NextResponse.next();
--- packages/trpc/src/trpc.ts
+++ packages/trpc/src/trpc.ts
@@ -1,5 +1,8 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { initTRPC } from '@trpc/server';
 import { type CreateNextContextOptions } from '@trpc/server/adapters/next';
+// @ts-ignore
+import requestIp from 'request-ip';
 
 type CreateContextOptions = Record<string, never>;
 
@@ -8,6 +11,22 @@ export const createInnerTRPCContext = async (_opts: CreateContextOptions) => {
 };
 
 export const createTRPCContext = async (_opts: CreateNextContextOptions) => {
+  const { req } = _opts;
+  const detectedIp = requestIp.getClientIp(req);
+  console.log('ip', detectedIp);
+  // @ts-ignore
+  const forwarded = req.headers['x-forwarded-for'];
+  console.log('forwarded', forwarded);
+  // @ts-ignore
+  console.log('remoteAddress', req?.socket?.remoteAddress);
+  console.log('headers', req.headers);
+  console.log('headers str', JSON.stringify(req.headers));
+  const compIp =
+    typeof forwarded === 'string'
+      ? forwarded.split(/, /)[0]
+      : // @ts-ignore
+        req?.socket?.remoteAddress;
+  console.log('compIp', compIp);
   return createInnerTRPCContext({});
 };
 
