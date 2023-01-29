02edb3dcee2490bb6aa291896d9051d1cab639b1
Amit Singh <amitsingh5198@gmail.com>
2023-01-29 11:58:26 +0530

    test ip

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,8 +1,20 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+// @ts-ignore
+import requestIp from 'request-ip';
 
 export function middleware(req: NextRequest) {
   if (req.nextUrl.pathname === '/') {
+    const detectedIp = requestIp.getClientIp(req);
+    console.log('ip', detectedIp);
+    // @ts-ignore
+    // const forwarded = req.headers['x-forwarded-for'];
+    // const ip =
+    //   typeof forwarded === 'string'
+    //     ? forwarded.split(/, /)[0]
+    //     : req.socket.remoteAddress;
+    // console.log('ip', { ip });
     const { geo, nextUrl } = req;
     nextUrl.searchParams.set('country', geo?.country ?? 'IN');
     return NextResponse.rewrite(nextUrl);
