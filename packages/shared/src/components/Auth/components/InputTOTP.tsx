45574ca0d95dee829f611ed1a99a65b3174d57c0
Amit Singh <amitsingh5198@gmail.com>
2023-03-05 22:57:55 +0530

    Use pininput component

--- packages/shared/src/components/Auth/components/InputTOTP.tsx
+++ packages/shared/src/components/Auth/components/InputTOTP.tsx
@@ -1,4 +1,4 @@
-import { Loader, TextInput } from '@mantine/core';
+import { Flex, Loader, PinInput } from '@mantine/core';
 import { useState } from 'react';
 import { TOTP_LENGTH } from '../constants';
 
@@ -7,45 +7,31 @@ const InputTOTP = ({
 }: {
   handleVerify: (totp: string) => Promise<void>;
 }) => {
-  const [totp, setTOTP] = useState('');
-  const [isVerifying, setIsVerifying] = useState(false);
+  const [state, setState] = useState({ isVerifying: false, hasError: false });
 
-  const handleTOTPChange = async (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    const inputTotp = event.target.value?.slice(0, TOTP_LENGTH);
-    setTOTP(inputTotp);
+  const handleTOTPChange = async (inputTotp: string) => {
+    setState((prev) => ({ ...prev, hasError: false }));
     if (inputTotp?.length === TOTP_LENGTH) {
-      setIsVerifying(true);
+      setState((prev) => ({ ...prev, isVerifying: true }));
       await handleVerify(inputTotp);
-      setIsVerifying(false);
-      setTOTP('');
+      setState({ isVerifying: false, hasError: true });
     }
   };
 
   return (
-    <TextInput
-      data-autofocus
-      maw="12.5rem"
-      size="lg"
-      type="number"
-      onBlur={({ target }) => target.focus()}
-      label="Enter TOTP"
-      value={totp}
-      onChange={handleTOTPChange}
-      styles={{
-        input: {
-          paddingLeft: '1rem',
-          paddingRight: '1rem',
-          letterSpacing: '1rem',
-        },
-        label: {
-          fontSize: '0.9375rem',
-        },
-      }}
-      disabled={isVerifying}
-      rightSection={isVerifying ? <Loader variant="oval" size="xs" /> : null}
-    />
+    <Flex direction="column" gap="0.5rem">
+      <Flex gap="0.5rem">
+        Enter TOTP {state.isVerifying && <Loader variant="oval" size="xs" />}
+      </Flex>
+      <PinInput
+        autoFocus
+        length={6}
+        type="number"
+        onChange={handleTOTPChange}
+        disabled={state.isVerifying}
+        error={state.hasError}
+      />
+    </Flex>
   );
 };
 
