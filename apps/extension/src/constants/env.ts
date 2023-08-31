9bee815ad8b0c8c799e1a67585f9b008b56f041d
Amit Singh <amitsingh5198@gmail.com>
2023-08-31 19:33:15 +0530

    Update env.ts

--- apps/extension/src/constants/env.ts
+++ apps/extension/src/constants/env.ts
@@ -2,12 +2,8 @@ import { createEnv } from '@t3-oss/env-core';
 import { z } from 'zod';
 
 export const env = createEnv({
-  clientPrefix: '',
   server: {
     NODE_ENV: z.literal('development').or(z.literal('production')),
-    HOST_NAME: z.string(),
-  },
-  client: {
     HOST_NAME: z.string(),
   },
   runtimeEnv: process.env,
