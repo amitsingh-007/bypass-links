7392bdfca7bd3b8e9d5c13067692ccb003a98714
Amit Singh <amitsingh5198@gmail.com>
2024-01-15 16:28:37 +0530

    Check

--- apps/extension/src/index.tsx
+++ apps/extension/src/index.tsx
@@ -3,38 +3,38 @@ import '@mantine/core/styles.css';
 import '@mantine/dates/styles.css';
 import 'mantine-contextmenu/styles.css';
 import './index.css';
-import { mantineTheme } from '@bypass/shared';
-import { MantineProvider } from '@mantine/core';
-import { ContextMenuProvider } from 'mantine-contextmenu';
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import Global from './components/Global';
-import PopupRoutes from './components/PopupRoutes';
+// import { mantineTheme } from '@bypass/shared';
+// import { MantineProvider } from '@mantine/core';
+// import { ContextMenuProvider } from 'mantine-contextmenu';
+// import { StrictMode } from 'react';
+// import { createRoot } from 'react-dom/client';
+// import { BrowserRouter } from 'react-router-dom';
+// import Global from './components/Global';
+// import PopupRoutes from './components/PopupRoutes';
 import Logging from './logging';
-import DynamicProvider from './provider/DynamicProvider';
+// import DynamicProvider from './provider/DynamicProvider';
 
 Logging.init();
 
-const App = () => {
-  return (
-    <StrictMode>
-      <BrowserRouter>
-        <MantineProvider defaultColorScheme="dark" theme={mantineTheme}>
-          <ContextMenuProvider shadow="md" borderRadius="md">
-            <DynamicProvider>
-              <PopupRoutes />
-              <Global />
-            </DynamicProvider>
-          </ContextMenuProvider>
-        </MantineProvider>
-      </BrowserRouter>
-    </StrictMode>
-  );
-};
+// const App = () => {
+//   return (
+//     <StrictMode>
+//       <BrowserRouter>
+//         <MantineProvider defaultColorScheme="dark" theme={mantineTheme}>
+//           <ContextMenuProvider shadow="md" borderRadius="md">
+//             <DynamicProvider>
+//               <PopupRoutes />
+//               <Global />
+//             </DynamicProvider>
+//           </ContextMenuProvider>
+//         </MantineProvider>
+//       </BrowserRouter>
+//     </StrictMode>
+//   );
+// };
 
-const container = document.getElementById('root');
-if (container) {
-  const root = createRoot(container);
-  root.render(<App />);
-}
+// const container = document.getElementById('root');
+// if (container) {
+//   const root = createRoot(container);
+//   root.render(<App />);
+// }
