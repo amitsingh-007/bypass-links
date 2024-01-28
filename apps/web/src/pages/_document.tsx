3c7ac1b2d14bb6fed8df9ca3722032d34c0ee3d1
Amit Singh <amitsingh5198@gmail.com>
2024-01-28 22:58:36 +0530

    Fix font loading due to experimental edge

--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -5,6 +5,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Google+Sans:wght@100;200;300;400;500;700;900&display=swap"
           rel="stylesheet"
