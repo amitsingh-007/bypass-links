34ced58d0b9cf3b37c60f691f7cec5045abbfb77
Amit Singh <amitsingh5198@gmail.com>
2024-02-09 22:49:24 +0530

    Add feedback on forums button

--- apps/extension/src/HomePopup/components/OpenForumLinks/index.tsx
+++ apps/extension/src/HomePopup/components/OpenForumLinks/index.tsx
@@ -3,10 +3,9 @@ import useFirebaseStore from '@/store/firebase/useFirebaseStore';
 import { matchHostnames } from '@/utils/common';
 import { sendRuntimeMessage } from '@/utils/sendRuntimeMessage';
 import { getCurrentTab } from '@/utils/tabs';
-import { Button } from '@mantine/core';
 import useHistoryStore from '@store/history';
-import { memo, useCallback, useEffect, useState } from 'react';
-import { MdForum } from 'react-icons/md';
+import { memo, useEffect, useState } from 'react';
+import ButtonWithFeedback from './ButtonWithFeedback';
 
 const isCurrentPageForum = async (url = '') => {
   const hostname = url && new URL(url).hostname;
@@ -21,30 +20,26 @@ const OpenForumLinks = memo(function OpenForumLinks() {
     (state) => state.startHistoryMonitor
   );
   const isSignedIn = useFirebaseStore((state) => state.isSignedIn);
-  const [isFetching, setIsFetching] = useState(false);
-  const [currentTab, setCurrentTab] = useState<chrome.tabs.Tab | null>(null);
-  const [isActive, setIsActive] = useState(false);
-
-  const initCurrentTab = async () => {
-    const curTab = await getCurrentTab();
-    setCurrentTab(curTab);
-  };
+  const [currentTab, setCurrentTab] = useState<chrome.tabs.Tab>();
+  const [isForumPage, setIsForumPage] = useState(false);
 
   useEffect(() => {
+    const initCurrentTab = async () => {
+      const curTab = await getCurrentTab();
+      setCurrentTab(curTab);
+    };
     initCurrentTab();
   }, []);
 
-  const initIsActive = useCallback(async () => {
-    const isForum = isSignedIn && (await isCurrentPageForum(currentTab?.url));
-    setIsActive(isForum);
+  useEffect(() => {
+    const initIsActive = async () => {
+      const isForum = isSignedIn && (await isCurrentPageForum(currentTab?.url));
+      setIsForumPage(isForum);
+    };
+    initIsActive();
   }, [currentTab?.url, isSignedIn]);
 
-  useEffect(() => {
-    initIsActive();
-  }, [isSignedIn, currentTab, initIsActive]);
-
-  const handleClick = async () => {
-    setIsFetching(true);
+  const openForumlinks = async () => {
     startHistoryMonitor();
     const { forumPageLinks } = await sendRuntimeMessage({
       key: 'forumPageLinks',
@@ -54,21 +49,13 @@ const OpenForumLinks = memo(function OpenForumLinks() {
     forumPageLinks.forEach((url) => {
       chrome.tabs.create({ url, active: false });
     });
-    setIsFetching(false);
   };
 
   return (
-    <Button
-      radius="xl"
-      loading={isFetching}
-      disabled={!isActive}
-      onClick={handleClick}
-      rightSection={<MdForum />}
-      fullWidth
-      color="yellow"
-    >
-      Forum
-    </Button>
+    <ButtonWithFeedback
+      openAllLinks={openForumlinks}
+      isForumPage={isForumPage}
+    />
   );
 });
 
