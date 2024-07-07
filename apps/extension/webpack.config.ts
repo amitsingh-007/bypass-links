2bd438b02baa6a9e45d4874d796aa6731bdbeb68
Amit Singh <amitsingh5198@gmail.com>
2024-07-07 10:11:16 +0530

    Kinda fix eslint file

--- apps/extension/webpack.config.ts
+++ apps/extension/webpack.config.ts
@@ -12,25 +12,24 @@ import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MergeJsonWebpackPlugin from 'merge-jsons-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import { resolve } from 'path';
+import { dirname, resolve } from 'path';
 import ReactRefreshTypeScript from 'react-refresh-typescript';
 import TerserPlugin from 'terser-webpack-plugin';
 import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
+import { fileURLToPath } from 'url';
 import webpack, { Configuration, RuleSetRule } from 'webpack';
 import 'webpack-dev-server';
 import { env } from './src/constants/env.js';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const fileName = fileURLToPath(import.meta.url);
+const dirName = dirname(fileName);
 
 const { DefinePlugin, optimize } = webpack;
 
 const PATHS = {
-  ROOT: resolve(__dirname),
-  SRC: resolve(__dirname, 'src'),
-  EXTENSION: resolve(__dirname, 'build'),
+  ROOT: resolve(dirName),
+  SRC: resolve(dirName, 'src'),
+  EXTENSION: resolve(dirName, 'build'),
 };
 
 const { NODE_ENV, HOST_NAME } = env;
@@ -175,9 +174,9 @@ const config: Configuration = {
               transpileOnly: true,
               ...(!isProduction && {
                 getCustomTransformers: () => ({
-                  before: [
-                    !isProduction && ReactRefreshTypeScript(),
-                  ].filter(Boolean),
+                  before: [!isProduction && ReactRefreshTypeScript()].filter(
+                    Boolean
+                  ),
                 }),
               }),
             },
