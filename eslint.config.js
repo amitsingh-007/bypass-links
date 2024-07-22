75a47a0de60ed204f6aebe4357406b8eb9465abe
Amit Singh <amitsingh5198@gmail.com>
2024-07-22 19:40:08 +0530

    Fix eslint CI issues for typed checking (#3123)

--- eslint.config.js
+++ eslint.config.js
@@ -55,11 +55,7 @@ export default tseslint.config(
   {
     languageOptions: {
       parserOptions: {
-        project: [
-          './packages/*/tsconfig.json',
-          './apps/*/tsconfig.json',
-          './tsconfig.json',
-        ],
+        project: true,
         tsconfigRootDir: import.meta.dirname,
       },
     },
@@ -108,7 +104,7 @@ export default tseslint.config(
   //         './tsconfig.json',
   //         './packages/*/tsconfig.json',
   //         './apps/*/tsconfig.json',
-  //       ],
+  //       ] or project: true, ref: https://typescript-eslint.io/getting-started/typed-linting/
   //       tsconfigRootDir: import.meta.dirname,
   //     },
   //   },
