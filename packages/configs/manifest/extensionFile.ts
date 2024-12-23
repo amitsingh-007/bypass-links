53323c3e28f25926e035b2c22875b347876c8cf7
Amit Singh <amitsingh5198@gmail.com>
2024-12-23 14:58:16 +0530

    Distribute signed firefox xpi file

--- packages/configs/manifest/extensionFile.ts
+++ packages/configs/manifest/extensionFile.ts
@@ -2,16 +2,20 @@ import manifest from './manifest.base.json' assert { type: 'json' };
 
 const FILE_NAME = {
   prefix: 'bypass-links-',
-  suffix: '.zip',
+  chromeSuffix: '.zip',
+  firefoxSuffix: '.xpi',
 };
+
+const getSuffix = (isChrome: boolean) =>
+  isChrome ? FILE_NAME.chromeSuffix : FILE_NAME.firefoxSuffix;
 
 export const getExtVersion = () => manifest.version;
 
-export const getFileNameFromVersion = (extVersion: string) =>
-  `${FILE_NAME.prefix}${extVersion}${FILE_NAME.suffix}`;
+export const getFileNameFromVersion = (extVersion: string, isChrome: boolean) =>
+  `${FILE_NAME.prefix}${extVersion}${getSuffix(isChrome)}`;
 
-export const getVersionFromFileName = (fileName: string) =>
+export const getVersionFromFileName = (fileName: string, isChrome: boolean) =>
   fileName.slice(
     fileName.indexOf(FILE_NAME.prefix) + FILE_NAME.prefix.length,
-    fileName.lastIndexOf(FILE_NAME.suffix)
+    fileName.lastIndexOf(getSuffix(isChrome))
   );
