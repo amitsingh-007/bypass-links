07f17ef787bdc410d6f50cf6177f6dffd6aabbc6
Amit Singh <amitsingh5198@gmail.com>
2024-12-29 15:14:41 +0530

    Refactor scroll button

--- packages/shared/src/components/ScrollButton.tsx
+++ packages/shared/src/components/ScrollButton.tsx
@@ -6,52 +6,49 @@ import styles from './styles/ScrollButton.module.css';
 interface Props {
   itemsSize: number;
   onScroll: (itemNumber: number) => void;
-  minItemsReqToShow?: number;
 }
 
-export const ScrollButton = memo<Props>(
-  ({ itemsSize, onScroll, minItemsReqToShow = 0 }) => {
-    const buttonConfig = useMemo(
-      () => [
-        {
-          text: 'Top',
-          icon: BsArrowUp,
-          onClick: () => onScroll(0),
-        },
-        {
-          text: 'Bottom',
-          icon: BsArrowDown,
-          onClick: () => onScroll(itemsSize),
-        },
-      ],
-      [itemsSize, onScroll]
-    );
+export const ScrollButton = memo<Props>(({ itemsSize, onScroll }) => {
+  const buttonConfig = useMemo(
+    () => [
+      {
+        text: 'Top',
+        icon: BsArrowUp,
+        onClick: () => onScroll(0),
+      },
+      {
+        text: 'Bottom',
+        icon: BsArrowDown,
+        onClick: () => onScroll(itemsSize),
+      },
+    ],
+    [itemsSize, onScroll]
+  );
 
-    if (minItemsReqToShow > 0 && itemsSize <= minItemsReqToShow) {
-      return null;
-    }
-
-    return (
-      <Button.Group
-        orientation="vertical"
-        pos="fixed"
-        bottom="0.5625rem"
-        right="1rem"
-        className={styles.container}
-      >
-        {buttonConfig.map(({ icon: Icon, text, onClick }) => (
-          <Button
-            key={text}
-            color="violet"
-            size="compact-sm"
-            leftSection={<Icon />}
-            classNames={{ inner: styles.buttonInner }}
-            onClick={onClick}
-          >
-            {text}
-          </Button>
-        ))}
-      </Button.Group>
-    );
+  if (itemsSize === 0) {
+    return null;
   }
-);
+
+  return (
+    <Button.Group
+      orientation="vertical"
+      pos="fixed"
+      bottom="0.5625rem"
+      right="1rem"
+      className={styles.container}
+    >
+      {buttonConfig.map(({ icon: Icon, text, onClick }) => (
+        <Button
+          key={text}
+          color="violet"
+          size="compact-sm"
+          leftSection={<Icon />}
+          classNames={{ inner: styles.buttonInner }}
+          onClick={onClick}
+        >
+          {text}
+        </Button>
+      ))}
+    </Button.Group>
+  );
+});
