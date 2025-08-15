ab2ed2510f0c9e98066995dfc620b0a0754fef07
Amit Singh <amitsingh5198@gmail.com>
2025-08-16 01:20:14 +0530

    Fix zod mini build

--- packages/shared/src/components/Bookmarks/schema/index.ts
+++ packages/shared/src/components/Bookmarks/schema/index.ts
@@ -32,44 +32,48 @@ export const BookmarksAndPersonsValidationSchema = z
     bookmarks: BookmarksObjSchema,
     persons: PersonsSchema,
   })
-  .refine(
-    ({ bookmarks, persons }) => {
-      for (const [bmId, bmData] of Object.entries(bookmarks.urlList)) {
-        const isMismatch = bmData.taggedPersons.some(
-          (personId) => !persons[personId]
-        );
-        // Check if the bookmark is tagged to a missing person
-        if (isMismatch) {
-          console.error(`Bookmark ${bmId} is mismatch`, bmData);
-          return false;
+  .check(
+    z.refine(
+      ({ bookmarks, persons }) => {
+        for (const [bmId, bmData] of Object.entries(bookmarks.urlList)) {
+          const isMismatch = bmData.taggedPersons.some(
+            (personId) => !persons[personId]
+          );
+          // Check if the bookmark is tagged to a missing person
+          if (isMismatch) {
+            console.error(`Bookmark ${bmId} is mismatch`, bmData);
+            return false;
+          }
         }
-      }
-      return true;
-    },
-    { message: 'Bookmarks and persons data mismatch' }
+        return true;
+      },
+      { message: 'Bookmarks and persons data mismatch' }
+    )
   )
-  .refine(
-    ({ bookmarks }) => {
-      const allData = { ...bookmarks.urlList, ...bookmarks.folderList };
-      for (const [bmId, bmData] of Object.entries(allData)) {
-        // Ignore root folder
-        if (!bmData.parentHash) {
-          return true;
+  .check(
+    z.refine(
+      ({ bookmarks }) => {
+        const allData = { ...bookmarks.urlList, ...bookmarks.folderList };
+        for (const [bmId, bmData] of Object.entries(allData)) {
+          // Ignore root folder
+          if (!bmData.parentHash) {
+            return true;
+          }
+          const parentFolder = bookmarks.folders[bmData.parentHash];
+          // Check if the parent folder exists
+          if (!parentFolder) {
+            console.error(`Bookmark ${bmId} parent data not found`, bmData);
+            return false;
+          }
+          const isMissing = !parentFolder.some((item) => item.hash === bmId);
+          // Check if bookmark is referenced in some folder
+          if (isMissing) {
+            console.error(`Bookmark ${bmId} is missing`, bmData);
+            return false;
+          }
         }
-        const parentFolder = bookmarks.folders[bmData.parentHash];
-        // Check if the parent folder exists
-        if (!parentFolder) {
-          console.error(`Bookmark ${bmId} parent data not found`, bmData);
-          return false;
-        }
-        const isMissing = !parentFolder.some((item) => item.hash === bmId);
-        // Check if bookmark is referenced in some folder
-        if (isMissing) {
-          console.error(`Bookmark ${bmId} is missing`, bmData);
-          return false;
-        }
-      }
-      return true;
-    },
-    { message: 'Incorrect bookmarks data' }
+        return true;
+      },
+      { message: 'Incorrect bookmarks data' }
+    )
   );
