4b5a88a25d4f9ad43d3ac396c07fa5968c408983
Amit Singh <amitsingh5198@gmail.com>
2025-08-16 20:43:24 +0530

    Update vitest.config.ts

--- packages/trpc/vitest.config.ts
+++ packages/trpc/vitest.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vitest/config';
 import { getEnv } from './src/constants/env';
 
 const projectRoot = path.dirname(path.dirname(process.cwd()));
-dotenv.config({ path: `${projectRoot}/.env` });
+dotenv.config({ path: path.join(projectRoot, '.env') });
 
 const { VERCEL_ENV } = getEnv();
 const isDev = VERCEL_ENV === 'development';
